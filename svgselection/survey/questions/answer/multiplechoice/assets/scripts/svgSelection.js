cfe9781687765921a8039ad2d4c2e16cb4c9fa31
Florian Heller <florian.heller@uhasselt.be>
2020-03-05 10:28:28 +0100

    Here comes the magic…

--- svgselection/survey/questions/answer/multiplechoice/assets/scripts/svgSelection.js
+++ svgselection/survey/questions/answer/multiplechoice/assets/scripts/svgSelection.js
@@ -7,11 +7,19 @@
     Create date - 21/02/2020
 *****/
 
-$(document).on('ready pjax:scriptcomplete',function(){
-	svg = $("#svg-selection-image").getSVGDocument();
 
-	svg.$(".clickTarget").bind("click", function(event) {   
-		alert(event.target.id); 
+$(document).on('ready pjax:scriptcomplete',function(){
+	// Go through all selection svgs (there can be multiple on the same page)
+	$(".svg-selection-image").each(function(index,element) {
+		questionID = element.id.slice(0,-3);
+		//Just because the document DOM is loaded, doesn't mean the SVG DOM is available yet
+		element.addEventListener('load', function() {
+			$(".clickTarget", element.getSVGDocument()).each(function(i,e) {
+				$(e).bind("click", function() {
+	 				 $("#answer"+questionID+this.id).prop( "checked", !$("#answer"+questionID+this.id).prop('checked')); //Toggle the checkbox
+	 				 this.classList.toggle('selected');	//Highlight the selection
+				});
+			});	
+		}, true);
 	});
-	
 });
